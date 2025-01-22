8d8756ed1712c1b1fd8744a738adf40d45d6ab18
ellen4555 <ellenjoy2028@cs.sjcetpalai.ac.in>
2025-01-22 16:29:24 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  printf("Size of Integer=%ld",sizeof(int));
+  printf("\nSize of float=%ld",sizeof(float));
+  printf("\nSize of Double=%ld",sizeof(double));
+  printf("\nSize of char=%ld",sizeof(char));
+  printf("\nSize of short integer=%ld",sizeof(short int));
+  printf("\nSize of long integer=%ld",sizeof(long int));
+  
+  return 0;
+}
