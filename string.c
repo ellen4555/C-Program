48a200c3cbabf1b691615e22a2f4cd84d630a9ff
ellen4555 <ellenjoy2028@cs.sjcetpalai.ac.in>
2025-02-19 15:11:32 +0530

    Add files via upload

--- string.c
+++ string.c
@@ -1,11 +1,23 @@
 #include<stdio.h>
 int main(){
-  printf("Size of Integer=%ld",sizeof(int));
-  printf("\nSize of float=%ld",sizeof(float));
-  printf("\nSize of Double=%ld",sizeof(double));
-  printf("\nSize of char=%ld",sizeof(char));
-  printf("\nSize of short integer=%ld",sizeof(short int));
-  printf("\nSize of long integer=%ld",sizeof(long int));
-  
-  return 0;
+  char str1[20],temp;
+  int length,i;
+  printf("Enter a string:");
+  fgets(str1,sizeof(str1),stdin);
+  for(length=0;str1[length]!='\0';length++);
+    i=length-1;
+  for(int j=0;j<=length/2;j++){
+    temp=str1[i];
+    str1[i]=str1[j];
+    str1[j]=temp;
+    i--;
+  }
+  printf("Reversed string: \n");
+  puts(str1);
+    return 0;
 }
+    
+    
+  
+  
+
