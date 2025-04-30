cf7c81ef8ab48bfb4cbd800dad4363f74f2cf311
ellen4555 <ellenjoy2028@cs.sjcetpalai.ac.in>
2025-04-30 16:16:48 +0530

    Add files via upload

---
+++ allo3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int *array;
+  int n;
+  printf("Enter the no.of elements");
+  scanf("%d",&n);
+  array=(int *) calloc(n,sizeof(int));
+  printf("Enter the elements");
+  for(int i=0;i<n;i++){
+    scanf("%d",array+i);
+  }
+  for(int i=0;i<n;i++){
+    printf("%d",*(array+i));
+  }
+  free(array);
+  return 0;
+}
