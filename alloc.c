98131b5c96efb0414b4d2c1cfefea621e4c87b84
ellen4555 <ellenjoy2028@cs.sjcetpalai.ac.in>
2025-04-30 16:17:26 +0530

    Add files via upload

---
+++ alloc.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int *array;
+  int n;
+  printf("Enter the no.of elements");
+  scanf("%d",&n);
+  array=(int *) malloc(n*sizeof(int));
+  printf("Enter the elements");
+  for(int i=0;i<n;i++){
+    scanf("%d",array+i);
+  }
+  for(int i=0;i<n;i++){
+    printf("%d",*(array+i));
+  }
+  return 0;
+}
